0f706ed531e860d40b0714dc5bab2291f0052d76
Ahtnezev <41706384+Ahtnezev@users.noreply.github.com>
2025-04-01 22:49:51 -0600

    [F] error URL

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -7,7 +7,7 @@ class MovieMapper {
   static Movie movieDBToEntity(MovieMovieDB moviedb) => Movie(
     adult: moviedb.adult,
     backdropPath: (moviedb.backdropPath != '')
-      ? 'https://image/,tmdb.org/t/p/w500${moviedb.backdropPath}'
+      ? 'https://image.tmdb.org/t/p/w500${moviedb.backdropPath}'
       : 'https://media.wired.com/photos/5a0201b14834c514857a7ed7/master/pass/1217-WI-APHIST-01.jpg',
     genreIds: moviedb.genreIds.map( (e) => e.toString()).toList(),
     id: moviedb.id,
@@ -16,7 +16,7 @@ class MovieMapper {
     overview: moviedb.overview,
     popularity: moviedb.popularity,
     posterPath: (moviedb.posterPath != '')
-      ? 'https://image/,tmdb.org/t/p/w500${moviedb.posterPath}'
+      ? 'https://image.tmdb.org/t/p/w500${moviedb.posterPath}'
       : 'no-poster',
     releaseDate: moviedb.releaseDate,
     title: moviedb.title,
